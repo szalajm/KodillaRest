9f7a118d9801753ae372642f3468613a179abd29
szalajm <szalajm.neo@gmail.com>
2019-11-08 14:01:29 +0100

    32.1

--- src/main/java/com/crud/tasks/controller/StaticWebPageController.java
+++ src/main/java/com/crud/tasks/controller/StaticWebPageController.java
@@ -13,9 +13,9 @@ public class StaticWebPageController {
         model.put("variable", "My Thymeleaf variable");
         model.put("one", 1);
         model.put("two", 2);
-        model.put("equation_one", "2*2=");
-        model.put("equation_two", "2*2+2=");
-        model.put("equation_three", "2-2*2=");
+        model.put("equation_one", "2*2= ");
+        model.put("equation_two", "2*2+2= ");
+        model.put("equation_three", "2-2*2= ");
         return "index";
     }
 
