daf7d6780d627c7acdb434a9ee699c3d81e575d2
szalajm <szalajm.neo@gmail.com>
2019-11-12 18:39:30 +0100

    32.2

---
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -0,0 +1,39 @@
+package com.crud.tasks.service;
+
+import com.crud.tasks.config.AdminConfig;
+import com.crud.tasks.config.CompanyConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+@Service
+public class MailCreatorService {
+
+    @Autowired
+    AdminConfig adminConfig;
+
+    @Autowired
+    CompanyConfig companyConfig;
+
+    @Autowired
+    @Qualifier("templateEngine")
+    private TemplateEngine templateEngine;
+
+    public String buildTrelloCardEmail(String message){
+        Context context = new Context();
+        context.setVariable("message", message);
+        context.setVariable("task_url", "http://localhost:8888/crud");
+        context.setVariable("button", "Visit webstie");
+        context.setVariable("admin_name", adminConfig.getAdminName());
+        context.setVariable("goodbye_message", "Good bye!");
+        context.setVariable("preview_message", "This is test message");
+        context.setVariable("company_name", companyConfig.getCompanyName());
+        context.setVariable("company_mail", companyConfig.getEmail());
+        context.setVariable("company_phone", companyConfig.getPhoneNumber());
+        context.setVariable("company_details", companyConfig.getCompanyName() + " " + companyConfig.getEmail()+ " " + companyConfig.getPhoneNumber());
+
+        return templateEngine.process("mail/created-trello-card-mail", context);
+    }
+}
--- src/main/java/com/crud/tasks/service/SimpleEmailService.java
+++ src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -7,7 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMailMessage;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Service;
+
+import javax.mail.internet.MimeMessage;
 
 @Service
 public class SimpleEmailService {
@@ -16,6 +21,9 @@ public class SimpleEmailService {
 
     @Autowired
     private JavaMailSender javaMailSender;
+
+    @Autowired
+    private MailCreatorService mailCreatorService;
 
     public void send(final Mail mail) {
         LOGGER.info("Starting email preperation....");
@@ -28,7 +36,7 @@ public class SimpleEmailService {
         }
     }
 
-    private SimpleMailMessage createMailMessage (final Mail mail){
+    /**private SimpleMailMessage createMailMessage (final Mail mail){
         SimpleMailMessage mailMessage = new SimpleMailMessage();
         mailMessage.setTo(mail.getMailTo());
         if(mail.getToCc() != null) {
@@ -36,6 +44,30 @@ public class SimpleEmailService {
         }
         mailMessage.setSubject(mail.getSubject());
         mailMessage.setText(mail.getMessage());
+        return mailMessage;
+    }
+     }
+     }
+     }
+     }
+     }
+     }
+     */
+
+    private MimeMessagePreparator createMimeMessage(final Mail mail){
+        return mimeMessage -> {
+            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
+            messageHelper.setTo(mail.getMailTo());
+            messageHelper.setSubject(mail.getSubject());
+            messageHelper.setText(mailCreatorService.buildTrelloCardEmail(mail.getMessage()), true);
+        };
+    }
+
+    private SimpleMailMessage createMailMessage (final Mail mail){
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(mail.getMailTo());
+        mailMessage.setSubject(mail.getSubject());
+        mailMessage.setText(mailCreatorService.buildTrelloCardEmail(mail.getMessage()));
         return mailMessage;
     }
 }
