f18b2bad54e2ea8a617add21f176c533169cb38d
szalajm <szalajm.neo@gmail.com>
2019-11-19 06:52:11 +0100

    Scheduler Szablon

--- src/main/java/com/crud/tasks/service/EmailSchedulerCreatorService.java
+++ src/main/java/com/crud/tasks/service/EmailSchedulerCreatorService.java
@@ -9,6 +9,9 @@ import org.springframework.stereotype.Component;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Component
 public class EmailSchedulerCreatorService {
 
@@ -27,15 +30,20 @@ public class EmailSchedulerCreatorService {
     private TemplateEngine templateEngine;
 
     public String buildSchedulerMail(String message){
+
+        List<String> functionality = new ArrayList<>();
+        functionality.add("You can add task");
+        functionality.add("Connection with Trello Boards");
+        functionality.add("Sendingtasks to Trello");
+
         Context context = new Context();
         context.setVariable("message", message);
         context.setVariable("task_url", "http://localhost:8888/tasks_frontend");
         context.setVariable("button", "See task repository");
-        context.setVariable("welcome_message", "Hallo");
-        context.setVariable("goodbye_message", "Bye");
-        context.setVariable("admin_name", adminConfig.getAdminName());
         context.setVariable("show_button", false);
-        context.setVariable("is_friend", false);
+        context.setVariable("admin_config", adminConfig);
+        context.setVariable("goodbye_message", "Bye");
+        context.setVariable("functionality", functionality);
         context.setVariable("company_details", companyConfig.getCompanyName() + " " + companyConfig.getEmail()+ " " + companyConfig.getPhoneNumber());
         return templateEngine.process("mail/email-scheduler", context);
     }
